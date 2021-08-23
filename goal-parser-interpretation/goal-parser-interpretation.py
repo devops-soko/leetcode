f109f2372619113f5a2d7548ba1b78f45546ff6a
devops-soko <79248512+devops-soko@users.noreply.github.com>
2021-08-24 00:58:07 +0900

    Time: 28 ms (80.15%), Space: 14.1 MB (67.89%) - LeetHub

--- goal-parser-interpretation/goal-parser-interpretation.py
+++ goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,7 +1,3 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        if len(command) <1 or len(command) >100:
-            return
-        if command.replace("G","").replace("()","").replace("(al)","") != "":
-            return
         return command.replace("(al)", "al").replace("()", "o")
