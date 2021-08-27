d433813de1aaae6f7d8628b7e7be1e98b5a0ea5c
devops-soko <79248512+devops-soko@users.noreply.github.com>
2021-08-27 09:59:57 +0900

    Time: 32 ms (54.80%), Space: 14.2 MB (49.65%) - LeetHub

--- reformat-date/reformat-date.py
+++ reformat-date/reformat-date.py
@@ -1,7 +1,10 @@
-import datetime
 class Solution:
     def reformatDate(self, date: str) -> str:
-        month_dic ={"Jan" : 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+        month_dic ={"Jan" : "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+        output = ''
         dd, mm, yyyy = date.split(' ')
-        x= datetime.datetime(int(yyyy), month_dic[mm], int(dd[:-2]))
-        return x.strftime("%Y-%m-%d")
+        if int(dd[:-2]) <10 :
+            output = yyyy + '-' + month_dic[mm] + '-0' + dd[:-2]
+        else :
+            output = yyyy + '-' + month_dic[mm] + '-' + dd[:-2]
+        return output
