d9414e02b9a9bb40d2316ff3d7d979e76803aa58
devops-soko <79248512+devops-soko@users.noreply.github.com>
2021-08-28 12:49:44 +0900

    Time: 65 ms (8.50%), Space: 14.3 MB (38.02%) - LeetHub

--- reformat-the-string/reformat-the-string.py
+++ reformat-the-string/reformat-the-string.py
@@ -1,27 +1,23 @@
 class Solution:
     def reformat(self, s: str) -> str:
-        
-        alpha = []
-        digits = []
-        for letter in s:
-            if letter.isalpha():
-                alpha.append(letter)
+        digit_list, char_list = [], []
+        for c in s :
+            if c.isdigit() :
+                digit_list.append(c)
             else:
-                digits.append(letter)
-                
-        answer = []
-        useAlpha: bool = len(alpha) > len(digits)
-        
-        while True:
-            if useAlpha:
-                if not alpha:
-                    break
-                answer.append(alpha.pop())
-            else:
-                if not digits:
-                    break
-                answer.append(digits.pop())
+                char_list.append(c)
 
-            useAlpha = not useAlpha
-        
-        return answer if not alpha and not digits else ""
+        if (len(digit_list) ==0 and len(char_list) !=1) or (len(digit_list) !=1 and len(char_list) ==0):
+            return ""
+
+        output = ""
+
+        for k in range(min([len(digit_list), len(char_list)])):
+            output += char_list.pop() + digit_list.pop()
+
+        if len(digit_list)> len(char_list) : 
+            return digit_list.pop() + output 
+        elif len(digit_list) < len(char_list) :
+            return output + char_list.pop()
+        else: 
+            return output
