0c92595133df0fff6c2dd7215455e004274e960e
devops-soko <79248512+devops-soko@users.noreply.github.com>
2021-08-28 22:35:24 +0900

    Time: 46 ms (6.26%), Space: 14.2 MB (56.50%) - LeetHub

--- word-pattern/word-pattern.py
+++ word-pattern/word-pattern.py
@@ -2,14 +2,14 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         default_dict = collections.defaultdict(str)
         word_list = s.split(' ')
-        for c, w in zip(pattern,word_list) :
-            if default_dict[c] == '' and  w not in default_dict.values():
-                default_dict[c] = w
-
-        pattern_word = ''
-        for c in pattern :
-            pattern_word += default_dict[c] + ' '
-        if pattern_word == s + ' ' :
-            return True
-        else :
+        if len(set(pattern)) != len(set(word_list)) or len(pattern) != len(word_list):
             return False
+
+        for i in range(len(pattern)) :
+            if default_dict[pattern[i]] == '' and  word_list[i]:
+                default_dict[pattern[i]] = word_list[i]
+
+            if default_dict[pattern[i]] != word_list[i] : 
+                return False
+
+        return True
