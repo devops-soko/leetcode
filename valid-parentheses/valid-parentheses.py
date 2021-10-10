2e26710ab42c5590f3a4b8552e63445476838db5
devops-soko <79248512+devops-soko@users.noreply.github.com>
2021-10-10 15:17:41 +0900

    Time: 32 ms (71.83%), Space: 14.4 MB (33.67%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,32 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        i =1
-        length = len(s)
-        while i < length  :
-            if s[i] == ')' :
-                if s[i-1] == '(' :
-                    s = s[0:i-1] + s[i+1:]
-                    if i !=1:
-                        i -=1
-                    length -=2
-                    continue
-            elif s[i] == ']' :
-                if s[i-1] == '[' :
-                    s = s[0:i-1] + s[i+1:]
-                    if i !=1:
-                        i -=1
-                    length -=2
-                    continue
-            elif s[i] == '}' :
-                if s[i-1] == '{' :
-                    s = s[0:i-1] + s[i+1:]
-                    if i !=1:
-                        i -=1
-                    length -=2
-                    continue
-            i+=1
+        dic = {"(":")", "{":"}","[":"]"}
+        stack = []
 
-        if s == '' :
-            return True
-        else:
-            return False
+        for c in s :
+            if c in dic :
+                stack.append(c)
+            else :
+                if len(stack) ==0  or dic[stack.pop()] != c:
+                    return False
+        return stack == []
+    
